44096e66639fd9b866b4abf340ad660fee795743
Just S1 <hinchanritheavuth20@kit.edu.kh>
2022-02-15 23:48:12 +0700

    Finished merging code

--- src/app/country-dashboard/country-dashboard.component.ts
+++ src/app/country-dashboard/country-dashboard.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { CountryService } from 'src/services/country.service';
+import { Country } from 'src/models/country';
 
 @Component({
   selector: 'app-country-dashboard',
@@ -7,9 +11,89 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CountryDashboardComponent implements OnInit {
 
-  constructor() { }
+  countryInfo !: FormGroup
+  countryObj:Country = new Country();
+  countryArray:Country[] = [];
+  addVisible !: boolean;
+  updateVisible !: boolean;
+  constructor(private formbuilder:FormBuilder, private countryApi:CountryService, private router: Router) { }
 
   ngOnInit(): void {
+    this.countryInfo = this.formbuilder.group({
+      id: ['', Validators.required],
+      name: ['', Validators.required],
+      code: ['', Validators.required],
+    })
+    this.getAllCountry();
   }
 
+  logout () {
+    this.router.navigate(['/login'])
+  }
+
+  addNewCountry() {
+    this.countryObj.id = this.countryInfo.value.id;
+    this.countryObj.name = this.countryInfo.value.name;
+    this.countryObj.code = this.countryInfo.value.code;
+    this.countryApi.addCountry(this.countryObj).subscribe(res => {
+      alert('Country Added Successfully')
+      this.getAllCountry();
+      this.countryInfo.reset();
+      this.router.navigate(['/country-dashboard'])
+    }, err => {
+      alert('Something went wrong')
+    })
+  }
+
+  getAllCountry() {
+    this.countryApi.getCountry().subscribe(res => {
+      this.countryArray = res;
+    })
+  }
+
+  deleteCountry(country: Country) {
+    this.countryApi.deleteCountry(country.id).subscribe(res => {
+      alert('Country Deleted Successfully')
+      this.getAllCountry();
+    })
+  }
+
+  editCountry(country: Country) {
+    this.addVisible = false;
+    this.updateVisible = true;
+    this.countryInfo.controls['id'].setValue(country.id);
+    this.countryInfo.controls['name'].setValue(country.name);
+    this.countryInfo.controls['code'].setValue(country.code);
+  }
+
+  updateCountry() {
+    this.countryObj.id = this.countryInfo.value.id;
+    this.countryObj.name = this.countryInfo.value.name;
+    this.countryObj.code = this.countryInfo.value.code;
+    this.countryApi.editCountry(this.countryObj).subscribe(res => {
+      alert('Country Updated Successfully')
+      this.getAllCountry();
+      this.countryInfo.reset();
+      this.router.navigate(['/country-dashboard'])  
+    })
+  }
+
+  disableUpdateButton() {
+    this.addVisible = true;
+    this.updateVisible = false;  
+  }
+
+  // disableAddButton() {
+  //   this.addVisible = false;
+  //   this.updateVisible = true;
+  // }
+
+  deleteAllCountry() {
+    for (let i = 0; i < this.countryArray.length; i++) {
+      this.countryApi.deleteCountry(this.countryArray[i].id).subscribe(res => {
+        this.getAllCountry();
+      })
+      alert('Country Deleted Successfully')
+    }
+  }
 }
--- src/services/country.service.ts
+++ src/services/country.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Country } from 'src/models/country';
-
+import { map } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
@@ -9,12 +9,23 @@ export class CountryService {
 
   constructor(private http: HttpClient) { }
 
-  getCountries() {}
+  getCountry() {
+    return this.http.get<Country[]>('http://localhost:3000/country').pipe(map(res => {return res}));
+  }
 
-  addCountry(country: Country) {}
+  addCountry(country : Country) {
+    return this.http.post<Country>('http://localhost:3000/country', country).pipe(map(res => {return res}));
+  }
 
-  editCountry(country: Country) {}
+  editCountry(country : Country) {
+    return this.http.put<Country>('http://localhost:3000/country/' + country.id, country).pipe(map(res => {return res}));
+  }
 
-  deleteCountry(country: Country) {}
+  deleteCountry(id : string) {
+    return this.http.delete<Country>('http://localhost:3000/country/' + id).pipe(map(res => {return res}));
+  }
 
+  deleteAllCountry() {
+    return this.http.delete<Country[]>('http://localhost:3000/country').pipe(map(res => {return res}));
+  }
 }
